d2d887418cdaa35823f757657f7f6ff772c447e7
rainvice <3218229723@qq.com>
2024-05-21 22:25:51 +0800

    完善节点信息

--- library/src/main/ets/Markdown/MarkdownNode.ts
+++ library/src/main/ets/Markdown/MarkdownNode.ts
@@ -9,20 +9,42 @@ export enum Type {
   LineBreak = 'lineBreak', // 换行
   Emphasis = 'emphasis', // 强调
   Quote = 'quote', // 引用
-  List  = 'list', // 列表
-  ListItem   = 'listItem', // 列表子项
+  List = 'list', // 列表
+  ListItem = 'listItem', // 列表子项
+  InlineCode = 'inlineCode', // 行内代码
+  CodeBlock = 'codeBlock', // 代码块
+  Divider = 'divider', // 分割线
+  Link = 'link', // 超链接
+  LinkQuote = 'linkQuote', // 连接引用
+  Image = 'image', // 图片
+  HTML = 'html', // 内嵌 html
+  Table = 'table', // 表格
+  DeleteLine = 'deleteLine', // 删除线
+  TaskList = 'taskList', // 任务列表
 }
 
 
-export enum EmphasisType{
+export enum EmphasisType {
   Bold = "bold",
   Italic = "italic"
 }
 
 
-export enum ListType{
+export enum ListType {
   Order = "order",
   UnOrder = "unOrder"
+}
+
+export enum LinkType {
+  Default = "default",
+  Quote = "quote"
+}
+
+
+export enum TableLayout {
+  Start = "start",
+  End = "end",
+  Center = "center"
 }
 
 /**
@@ -40,7 +62,7 @@ export type Level = 1 | 2 | 3 | 4 | 5 | 6
 export abstract class Node {
   type: Type;
   children: Node[] = [];
-  attributes?: { [name: string]: string }
+  attributes?: { [name: string]: string | number }
   text?: string
   parent?: Node
 
@@ -75,16 +97,16 @@ export class Text extends Node {
 
 export class Header extends Node {
   type: Type = Type.Header
-  level: Level = 1;
 
   /**
    *
    * @param text 标签内未解析的文本
    * @param level 标题等级
    */
-  constructor(text: string, level: Level) {
+  constructor(text: string, level: Level, id: string = null) {
     super(text)
-    this.level = level
+    this.attributes['level'] = level
+    this.attributes['id'] = id
   }
 }
 
@@ -98,15 +120,13 @@ export class LineBreak extends Node {
 }
 
 export class Emphasis extends Node {
-
   type: Type = Type.Emphasis
   emphasisType: EmphasisType[]
 
-  constructor(text:string, ...emphasisTypes: EmphasisType[]) {
+  constructor(text: string, ...emphasisTypes: EmphasisType[]) {
     super(text);
     this.emphasisType = emphasisTypes
   }
-
 }
 
 export class Quote extends Node {
@@ -122,15 +142,141 @@ export class List extends Node {
     super(text)
     this.listType = listType
   }
+
   appendChild(child: ListItem): void {
     child.parent = this
     this.children.push(child)
   }
 }
 
-export class ListItem extends Node{
+export class ListItem extends Node {
   type: Type = Type.ListItem
 }
+
+export class InlineCode extends Node {
+  type: Type = Type.InlineCode
+}
+
+
+export class CodeBlock extends Node {
+  type: Type = Type.CodeBlock
+
+  constructor(text: string, langd: string) {
+    super(text)
+    this.attributes['langd'] = langd
+  }
+}
+
+
+export class Divider extends Node {
+  type: Type = Type.Divider
+}
+
+/**
+ * todo link 中可以使用行内代码以及图片
+ */
+export class Link extends Node {
+  type: Type = Type.Link
+  linkType: LinkType = LinkType.Default
+  url: string = ''
+  name: string = ''
+  title: string = ''
+
+  /**
+   *
+   * @param url 链接，linkType 为引用时为引用位置
+   * @param name
+   * @param title
+   * @param linkType
+   */
+  constructor(url: string, name: string, title: string, linkType: LinkType = LinkType.Default) {
+    super(null)
+    this.attributes['url'] = url
+    this.attributes['name'] = name
+    this.attributes['title'] = title
+    this.linkType = linkType
+  }
+}
+
+export class LinkQuote extends Node {
+  type: Type = Type.LinkQuote
+
+  /**
+   *
+   * @param url url 地址
+   * @param tag 引用名
+   * @param title 标题
+   */
+  constructor(url: string, tag: string, title: string) {
+    super(null)
+    this.attributes['url'] = url
+    this.attributes['tag'] = tag
+    this.attributes['title'] = title
+  }
+}
+
+
+export class Image extends Node {
+  type: Type = Type.Image
+
+  constructor(url: string, name: string, title: string) {
+    super(null)
+    this.attributes['url'] = url
+    this.attributes['name'] = name
+    this.attributes['title'] = title
+  }
+}
+
+export class HTML extends Node {
+  type: Type = Type.HTML
+}
+
+
+export class Table extends Node {
+  type: Type = Type.Table
+  header: Paragraph[] = []
+  layout: TableLayout[] = []
+  body: Paragraph[][] = []
+
+  constructor(header: Paragraph[], layout: TableLayout[], body: Paragraph[][]) {
+    super(null)
+    this.header = header
+    this.layout = layout
+    this.body = body
+  }
+
+  appendRow(...row: Paragraph[]) {
+    this.body.push(row)
+  }
+}
+
+
+
+export class DeleteLine extends Node {
+  type: Type = Type.DeleteLine
+}
+
+export class TaskList extends Node {
+  type: Type = Type.TaskList
+  item: string[] = []
+  checked: number[] = []
+
+  constructor(item: string[], checked: number[]) {
+    super(null)
+    this.item = item
+    this.checked = checked
+  }
+}
+
+
+// ————————————————————————————
+// todo 脚注
+// todo 定义列表
+// todo 使用 Emoji 表情
+// todo 自动网址链接
+// todo 转义字符语法
+// ————————————————————————————
+
 
 
 
